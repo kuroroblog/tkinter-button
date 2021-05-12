a85431495501291ad466a6404be16f37ac728b85
tocchimittsu <arimagedon21@gmail.com>
2021-05-12 18:22:00 +0900

    修正

---
+++ README.md
@@ -0,0 +1,5 @@
+# tkinter-frame
+Tkinterのflameについてのコード
+
+# 参考記事
+https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
---
+++ main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+
+
+class Application(tk.Frame):
+    # ボタンがクリックされた場合に実行される関数
+    def clickCommand(self):
+        print("ボタンがクリックされました。")
+
+    def __init__(self, master=None):
+        # Windowの初期設定を行う。
+        super().__init__(master)
+
+        # Windowの画面サイズを設定する。
+        self.master.geometry("300x200")
+
+        # Windowを親要素として、frame Widget(Frame)を作成する。
+        frame = tk.Frame(self.master)
+
+        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        frame.pack()
+
+        # frame Widget(Frame)を親要素として、button Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅の設定
+        # height : 高さの設定
+        button = tk.Button(frame, text="button",
+                         width=10, height=5)
+
+        # frame Widget(Frame)を親要素とした場合に、button Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        button.pack()
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    # Windowをループさせて、継続的にWindow表示させる。
+    app.mainloop()
